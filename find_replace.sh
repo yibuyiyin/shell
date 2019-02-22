8a95d6c07b541168b0e15f368f5449f406682cc1
peng.yu <peng.yu@qjfu.cn>
2019-02-22 17:18:35 +0800

    新增搜索与替换shell脚本

---
+++ find_replace.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ -z "$1" -o -z "$2" ];then
+    echo 'usage rgrep [find] [replace]'
+    exit 1
+fi
+grep "$1" -r --color .
+echo "$1 be replaced by $2"
+read -t 30 -n 3 -p "replace string,need confirm(y/n):" ret 
+if [ -z "$ret" ];then
+    ret='n'
+fi
+if test $ret = 'y'; then
+    grep "$1" -r .|awk -F : '{print $1}'|uniq|xargs sed -i "s#$1#$2#g"
+else
+    echo 'cancel relace'
+fi  
