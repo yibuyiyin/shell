9b0054b409a68e951eb453814c2c493dffa8cfff
peng.yu <peng.yu@yibuyiyin.com>
2019-05-25 12:51:41 +0800

    itsos_replace add args path

--- itsos_replace.sh
+++ itsos_replace.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-if [ -z "$1" -o -z "$2" ];then
-    echo 'usage rgrep [find] [replace]'
+if [ -z "$1" -o -z "$2" -o -z "$3" ];then
+    echo 'usage cmd [find] [replace] [path]'
     exit 1
 fi
-grep "$1" -r --color .
+grep -r --color "$1" $3
 echo "$1 be replaced by $2"
-read -t 30 -n 3 -p "replace string,need confirm(y/n):" ret 
-if [ -z "$ret" ];then
-    ret='n'
+read -t 30 -n 3 -p "replace string,need confirm(y/n):" state 
+if [ -z "$state" ];then
+    state='n'
 fi
-if test $ret = 'y'; then
-    grep "$1" -r .|awk -F : '{print $1}'|uniq|xargs sed -i "s#$1#$2#g"
+if test $state = 'y'; then
+    grep -r "$1" $3|awk -F : '{print $1}'|uniq|xargs sed -i "s#$1#$2#g"
 else
     echo 'cancel relace'
 fi  
